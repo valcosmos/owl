01989e64325ceb52e46c3654d04374d4178db7c9
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2023-06-25 13:55:15 +0800

    feat: enable versioning

--- apps/devops/src/main.ts
+++ apps/devops/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DevopsModule } from './devops.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(DevopsModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3001);
   console.log('Devops is running on ==> http://localhost:3001');
 }
--- apps/owl/src/main.ts
+++ apps/owl/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
   console.log('Owl is running on ==> http://localhost:3000');
 }
