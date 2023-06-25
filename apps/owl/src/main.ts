261138e09bed19a2ec9d7972da2235143fc403e7
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2023-06-25 14:01:47 +0800

    feat: add common data struct

--- apps/owl/src/main.ts
+++ apps/owl/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
+    defaultVersion: '1',
     type: VersioningType.URI,
   });
+
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
   console.log('Owl is running on ==> http://localhost:3000');
